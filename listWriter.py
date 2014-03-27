2c037ceb460eb6d558c12e715c2d9aaafd229286
Sasha Ehrenstein <inquisitorsasha@sturmkrieg.ru>
2014-03-27 19:38:47 -0400

    Adding the script to the repository

---
+++ listWriter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import itertools
+
+minLen = int(raw_input('Enter the minimum word length: '));
+maxLen = int(raw_input('Enter the maximum word length: '));
+simpList = raw_input("Generate simple wordlist? (y/n): ");
+if simpList == 'y':
+	charSet = 'abcdefghijklmnopqrstuvwxyz';
+elif simpList == 'n':
+	charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+';
+listName = raw_input('Name for the wordlist file to be created: ');
+
+for l in range(minLen,maxLen):
+	word = itertools.permutations(charSet, l)
+
+	for i in word:
+		file = open(listName, 'a');
+		file.write(''.join(i)+'\n');
+		file.close();
